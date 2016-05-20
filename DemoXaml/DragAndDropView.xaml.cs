c1625f91b45f484838f872d5cc26aadb36e2d56b
Daztane <neyzou@hotmail.fr>
2016-05-20 13:55:29 +0100

    chore(xaml): first commit
    Create the first commi

---
+++ DemoXaml/DragAndDropView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace DemoXaml
+{
+    /// <summary>
+    /// Interaction logic for DragAndDropView.xaml
+    /// </summary>
+    public partial class DragAndDropView : Window
+    {
+        public DragAndDropView(string message)
+        {
+            InitializeComponent();
+
+            NamedTextBox.Text = message;
+        }
+    }
+}
---
+++ DemoXaml/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoXaml
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// On user click on edit button
+        /// </summary>
+        /// <param name="sender">The edit button</param>
+        /// <param name="e">Button parameters</param>
+        private void OnEditClick(object sender, RoutedEventArgs e)
+        {
+            NamedProgressBar.Visibility = Visibility.Visible;
+
+            var window = new DragAndDropView("Je suis le message") {Owner = this};
+            // subscribe to event
+            window.Unloaded += WindowOnUnloaded;
+            // Unsubscribe
+            // window.Unloaded -= WindowOnUnloaded;
+
+            window.Show();
+        }
+
+        private void WindowOnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var dragAndDropView = new DragAndDropView("Blblblblbl");
+            dragAndDropView.ShowDialog();
+        }
+    }
+}
